c2b1d2dbe1558fb80b6fa6123f1079ca9ea94a0d
george <sf17@sunnyhealthfitness.cn>
2024-12-26 13:33:20 +0800

    port change

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
   // app.useGlobalPipes(new ValidationPipe())
   app.useGlobalInterceptors(new TransformInterceptor())
   app.useGlobalFilters(new HttpExceptionFilter())
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
